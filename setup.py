726d504d4433272321f3600dcd89aad87993c927
git <it@img-srl.com>
2024-10-21 12:47:18 +0200

    remove dependencies

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='crudLib',
     packages=find_packages(include=['crudLib']),
-    version='2.1.0',
+    version='2.1.1',
     description='Mongo datalake management Python library',
     author='Nivetha Rajkumar',
-    install_requires=['uuid', 'pandas', 'pymongo']
+    install_requires=[]
 )
